f2263b2ea57978196670197eb90972b854fdb4fc
hozzjss <medo3578@gmail.com>
2017-01-29 19:08:02 +0200

    Fully functional PEMDAS Calc

--- app.js
+++ app.js
@@ -1,88 +1,115 @@
+// Fully functional PEMDAS calculator !!
+// DOCS
+// numberify
+// takes an string argument and returns the number equivalent
+// negative string numbers like "-1" are converted to their
+// negative equivalent without any modifications
+//
+// isLargeFloat
+// detects floating point issues
+//
+// chainCalculate
+// performs recursive calculation if there is more than one operator
+// MDAS in mind
+// goes through the regexes to detect Multiplication,
+// Division, Addition, or Subtraction in order.
+// After it finds one of the operations
+// it sends them to the calculate function for processing
+// then it replaces the result with the operation it has
+// processed
+// if no more operations regexes were detected the loop is
+// bypassed and the return statement executes
+//
+// init
+// clear screen if C and inits the calculation if not
+// if the init is called without an argument
+// it is considered to be a clearing command
+// thus now it's cleared and the calculation
+// is not initialized
+//  if an option is provided however,
+//   it inits the calculator with that option
+//   passed to the function
+//
+// appendOp
+// appends operators or operands
+//
+// calculate
+// the calculate function is ready to
+// process one operation at a time
+// it is a dependency of the
+// chainCalculate function
 "use strict";
+
+// Booleans ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 let isInit, calcDone, syntaxErr;
 
-// just (MDAS) for now
-// next version: Parentheses and Exponents
+// regexes ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+const operationRes = [
+  /(\-)?\d+(\.)?(\d+)?\^(\-)?\d+(\.)?(\d+)?/g,
+  /(\-)?\d+(\.)?(\d+)?\×(\-)?\d+(\.)?(\d+)?/g,
+  /(\-)?\d+(\.)?(\d+)?\÷(\-)?\d+(\.)?(\d+)?/g,
+  /(\-)?\d+(\.)?(\d+)?\+(\-)?\d+(\.)?(\d+)?/g,
+  /(\-)?\d+(\.)?(\d+)?\-(\-)?\d+(\.)?(\d+)?/g
+];
+
+const parensRe = /\((\-)?\d+(\.)?(\d+)?\D(\-)?\d+(\.)?(\d+)?\)/g
+
+// functions ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+const numberify = (a) => {return +a;};
+const power = (a, b) => {
+  if (b == 0) {
+    return 1;
+  }
+  return a * power(a, b -1);
+}
+const operationFuns = [
+  power,
+  (a, b) => {return a * b;},
+  (a, b) => {return a / b;},
+  (a, b) => {return a + b},
+  (a, b) => {return a - b;}
+];
+
+const isLargeFloat = (number) => {
+  number = number.toString();
+  if (number.length > 8 && number.indexOf(".") < 8) {
+    return true;
+  }
+  return false;
+};
 
 const chainCalculate = (operation) => {
-  const operationRes = [
-    /\d+(\.?)(\d+?)\×\d+(\.)?(\d+)?/g,
-    /\d+(\.?)(\d+?)\÷\d+(\.)?(\d+)?/g,
-    /\d+(\.?)(\d+?)\+\d+(\.)?(\d+)?/g,
-    /\d+(\.?)(\d+?)\-\d+(\.)?(\d+)?/g];
-
   for (let i = 0; i < operationRes.length; i++) {
     if (operation.match(operationRes[i])) {
       let currentOperation = operation.match(operationRes[i])[0];
       return chainCalculate(operation.replace(currentOperation, calculate(currentOperation)));
     }
+    operation = operation.replace("(", "").replace(")", "")
   }
   return operation;
 };
 
-
-// clear screen if C and inits the calculation if not
 const init = function (option) {
   if (!option) {
-    /* if the init is called without an argument
-      it is considered to be a clearing command
-      thus now it's cleared and the calculation
-      is not initialized */
-
     $(".display").text("0.");
     isInit = false;
   } else if (option == "Syntax Error!") {
     $(".display").text(option);
     syntaxErr = true;
   } else {
-    /* if an option is provided however,
-      it inits the calculator with that option
-      passed to the function */
-
     $(".display").text(option);
     isInit = true;
   }
 };
 
-// appends operators or operands
 const appendOp = function (op) {
   const display = $(".display").text();
   $(".display").text(display + op);
 };
 
+
 const calculate = function (operation) {
-
-  // basic operations add, subtract, multiply, and divide
-  const numberify = (a) => {
-    return +a;
-  };
-  const operationFuns = [
-    (a, b) => {
-      return a + b;
-    },
-    (a, b) => {
-      return a - b;
-    },
-    (a, b) => {
-      return a * b;
-    },
-    (a, b) => {
-      return a / b;
-    }
-    ];
-
-  // ye know =D
-  const operatorRes = [/\+/g, /\-/g, /\×/g, /\÷/g];
-
-  // detects floating point issues
-  const isLargeFloat = (number) => {
-    if (number.toString().length > 8 && number.toString().indexOf(".") < 8) {
-      return true;
-    }
-    return false;
-  };
-
-  // matching the regex found with the appropriate operation
+  const operatorRes = [/\^/g ,/\×/g, /\÷/g, /\+/g, /\-/g];
   for (let i = 0; i < operationFuns.length; i++) {
     if (operatorRes[i].test(operation)) {
       operation = operation.split(operatorRes[i]);
@@ -91,13 +118,29 @@ const calculate = function (operation) {
         operation = operation.toFixed(2);
         if (operation.split(".")[1].match(/^0+/g) > 0) {
           operation = parseInt(operation);
+          return operation;
         }
+        return operation;
       }
+      return operation;
     }
   }
-  return operation;
 };
 
+const isValidOp = (operation) => {
+  if (((operation.indexOf("(") == -1) && (operation.indexOf(")")!= -1)) ||
+  (((operation.indexOf("(") != -1) && (operation.indexOf(")") == -1)))) {
+    return false;
+  }
+  if (/\D{2,}/g.test(operation) &&
+  !/\D\-/g.test(operation)
+  || /\-\-/.test(operation)) {
+    return false;
+  }
+  return true;
+};
+
+// event handlers ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 $(".btn").click(function () {
   if (syntaxErr) {
     init();
@@ -107,7 +150,7 @@ $(".btn").click(function () {
 
 $(".calculate").click(function () {
   let operation = $(".display").text();
-  if (/\D{2,}/g.test(operation)) {
+  if (!isValidOp(operation)) {
     init("Syntax Error!");
     return false;
   }
@@ -121,6 +164,10 @@ $(".clear").click(function () {
 
 $(".delete").click(function () {
   let display = $(".display").text();
+  if (calcDone) {
+    init();
+    return false;
+  }
   if (display.length == 1 || display == "0.") {
     init();
     return false;
@@ -146,5 +193,28 @@ $(".number, .dot, .operation").click(function () {
     } else {
       init(that.innerHTML);
     }
+  }
+});
+
+// keyboard integration
+$(document).on("keypress", function(e) {
+  let key = String.fromCharCode(e.which);
+  const entries = [".", "+", "-", "*", "/", "=", "(", ")", "^"];
+  const entriesIds = ["dot", "add", "sub", "mul", "div", "cal", "lp", "rp", "pow"];
+  if (e.which == 13) {
+    $("#cal").click();
+    return false;
+  }
+  if (entries.indexOf(key) !== -1) {
+    $(`#${entriesIds[entries.indexOf(key)]}`).click();
+  } else if (/\d/g.test(key)) {
+    $(`#${key}`).click();
+  }
+});
+
+$(document).on("keydown", function (e) {
+  if (e.which == 8) {
+    $(".delete").click();
+    return false;
   }
 });
